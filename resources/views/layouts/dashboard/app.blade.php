042fecc6da7a72ac7e3b29e0b53cf011793a23c1
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-03-02 01:46:40 +0200

    4(إنشاء المودل والكونترولر للكاتيجوري + بعض التعديلات البسيطة)

--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -44,14 +44,8 @@
             {{--<li class="breadcrumb-item"><a href="#">Dashboard</a></li>--}}
         {{--</ul>--}}
     {{--</div>--}}
-    <h2>Dashboard</h2>
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item"><a href="#">Library</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Data</li>
-        </ol>
-    </nav>
+
+    @yield('content')
 
 </main>
 <!-- Essential javascripts for application to work-->
