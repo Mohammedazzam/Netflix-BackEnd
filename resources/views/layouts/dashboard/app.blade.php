45d3355881b17f04f0eb719b62ab163e6896b44f
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-03-01 18:38:06 +0200

    (الانتهاء من تنسيق المشروع وتركيب اللوحة وجاهزة الآن للعمل)3.3

--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -44,6 +44,14 @@
             {{--<li class="breadcrumb-item"><a href="#">Dashboard</a></li>--}}
         {{--</ul>--}}
     {{--</div>--}}
+    <h2>Dashboard</h2>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Library</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Data</li>
+        </ol>
+    </nav>
 
 </main>
 <!-- Essential javascripts for application to work-->
