1c7f7f25912be22e45c8bed32ec148af39fb8bb2
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-03-03 15:50:33 +0200

    4.5(عمل الأزرار في صفحة الإندكس للكاتيجوري)

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -12,4 +12,30 @@
         </ol>
     </nav>
 
+
+    <div class="tile mb-4">
+
+        <div class="row">
+            <div class="col-12">
+                <form action="">
+                    <div class="row">
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <input type="text" name="search" class="form-control" placeholder="search">
+                            </div>
+                        </div><!--end of col-->
+
+                        <div class="col-md-4">
+                            <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i>Search</button>
+                            <a href="" class="btn btn-primary"><i class="fa fa-plus"></i>Add</a>
+                        </div>
+                    </div><!--end of row-->
+                </form>
+
+            </div>
+
+        </div>
+    </div>
+
 @endsection
