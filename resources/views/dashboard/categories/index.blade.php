2ad955a462bd0c324220e8903c612e0b39bd1bf6
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-03-03 16:36:16 +0200

    4.6(عمل الفورم الخاص بالكاتيجوري في الاندكس وعدم الإظهار إذا فش داتا)

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -16,7 +16,9 @@
     <div class="tile mb-4">
 
         <div class="row">
+
             <div class="col-12">
+
                 <form action="">
                     <div class="row">
 
@@ -31,11 +33,50 @@
                             <a href="" class="btn btn-primary"><i class="fa fa-plus"></i>Add</a>
                         </div>
                     </div><!--end of row-->
-                </form>
 
+                </form><!--end of form-->
+
+            </div><!--end of col 12-->
+
+        </div><!--end of row-->
+
+        <div class="row">
+            <div class="col-md-12">
+                @if($categories->count()> 0)
+                    <table class="table table-hover">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                            @foreach($categories as $category)
+                                <tr>
+                                    <td>{{$index+1}}</td>
+                                    <td>{{$category->name}}</td>
+                                    <td>
+                                        <a href="{{route('dashboard.categories.edit',$category->id)}}"><i class="fa fa-edit"></i>Edit</a>
+                                        <form method="post" action="{{route('dashboard.categories.delete')}}">
+                                            @csrf
+                                            @method('delete')
+
+                                            <button type="submit" class="btn btn-danger"><i class="fa fa-trash"></i>Delete</button>
+                                        </form><!--end of form-->
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                @else
+                    <h3>Sorry No Data Records Found </h3>
+
+                @endif
             </div>
-
         </div>
-    </div>
+    </div><!--end of tile-->
 
 @endsection
