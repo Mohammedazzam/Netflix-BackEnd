4aafc1a703b787af089b4b332d6374beb5ad5797
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-03-03 16:55:15 +0200

    4.8(البدء في صفحة الكرييت)

--- resources/views/dashboard/categories/create.blade.php
+++ resources/views/dashboard/categories/create.blade.php
@@ -2,7 +2,35 @@
 
 
 @section('content')
+
+    <h2>Categories</h2>
+
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{route('dashboard.welcome')}}">Dashboard</a></li>
+            <li class="breadcrumb-item"><a href="{{route('dashboard.categories.index')}}">Categories</a></li>
+            <li class="breadcrumb-item active">Add</li>
+        </ol>
+    </nav>
+
+
     <div class="tile mb-4">
-        <h2>Welcome</h2>
+        <form method="post" action="{{'dashboard.categories.store'}}">
+            @csrf
+            @method('post')
+
+            <div class="form-group">
+
+                <label>Name</label>
+                <input type="text" name="name" class="form-control">
+
+            </div>
+
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary"><i class="fa fa-plus"></i> Add</button>
+            </div>
+
+        </form>
     </div><!--end of tile-->
 @endsection
