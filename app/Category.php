aeac7102b8d246d78a0f5e4d1dd6f01887cfe340
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-03-03 22:28:28 +0200

    5.4(لعمل أول حرف كابتال )

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable =['name'];
-}
+
+
+    //attributes-----------
+
+    public function getNameAttribute($value){
+
+        return ucfirst($value);
+
+    }//end of getNameAttribute هذه لجعل أول حرف كابتل
+
+
+}//end of model
